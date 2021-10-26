5ec9672dfa1ba766472f661541eabfd8c4e12d2a
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-10-26 18:22:07 +0000

    Implement flatten

---
+++ flatten.js
@@ -0,0 +1,36 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("The arrays are equal.");
+  } else {
+    console.log("The arrays are not equal.");
+  }
+};
+
+const flatten = function(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Array.isArray(arr[i])) {
+      for (let j = 0; j < arr[i].length; j++) {
+        result.push(arr[i][j]);
+      }
+    } else {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]]));
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
