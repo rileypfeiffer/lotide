b738eacd84242168a4f7eadc78c1874f1cfac2ea
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-10-26 17:12:11 +0000

    Implement assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,21 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("The arrays are equal.");
+  } else {
+    console.log("The arrays are not equal.");
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
