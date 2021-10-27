9dfb25635a28d90bbc6d484abbb33b596a7a8668
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-10-27 17:55:27 +0000

    Implement countLetters

---
+++ countLetters.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const letterCount = {};
+  for (const letter of sentence) {
+    if (letter !== " ") {
+      if (letterCount[letter]) {
+        letterCount[letter] += 1;
+      } else {
+        letterCount[letter] = 1;
+      }
+    }
+  }
+  return letterCount;
+};
+
+const result = countLetters("lighthouse in the house");
+
+assertEqual((result["l"]), 1);
+assertEqual((result["i"]), 2);
+assertEqual((result["g"]), 2);
