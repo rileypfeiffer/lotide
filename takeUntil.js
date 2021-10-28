775b3d1e917b17bfdd23908814398f0a63b2f493
Riley Pfeiffer <riley_pfeiffer@hotmail.com>
2021-10-28 18:12:31 +0000

    Implement takeUntil

---
+++ takeUntil.js
@@ -0,0 +1,42 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2) === true) {
+    console.log("The arrays are equal.");
+  } else {
+    console.log("The arrays are not equal.");
+  }
+};
+
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (let val of array) {
+    if (callback(val)) {
+      return result;
+    }
+    result.push(val);
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
